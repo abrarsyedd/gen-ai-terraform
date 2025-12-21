35a7123b1f6fc72cd0b5d4802489663191262da2
Abrar Syed <sabrarsayeed@gmail.com>
2025-12-21 18:31:15 +0000

    provisioning GenAI server

--- main.tf
+++ main.tf
@@ -68,10 +68,6 @@ output "ec2_public_ip_06f035"    { value = module.ec2_instance_06f035.public_ip 
 output "ec2_private_ip_06f035"   { value = module.ec2_instance_06f035.private_ip }
 
 
-provider "aws" {
-  region = "us-east-1"
-}
-
 module "keypair_5cbe06" {
   source                  = "./modules/keypair_5cbe06"
   project_name            = "GenAI"
@@ -104,3 +100,41 @@ module "ec2_instance_5cbe06" {
 output "ec2_instance_id_5cbe06" { value = module.ec2_instance_5cbe06.instance_id }
 output "ec2_public_ip_5cbe06"    { value = module.ec2_instance_5cbe06.public_ip }
 output "ec2_private_ip_5cbe06"   { value = module.ec2_instance_5cbe06.private_ip }
+
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+module "keypair_f8bedb" {
+  source                  = "./modules/keypair_f8bedb"
+  project_name            = "GenAI"
+  environment             = "dev"
+  name                    = "GenAI-keypair-f8bedb"
+}
+
+module "ec2_instance_f8bedb" {
+  source                  = "./modules/ec2_f8bedb"
+  project_name            = "GenAI"
+  environment             = "dev"
+  name                    = "GenAI-ec2-f8bedb"
+  instance_type           = "t3.micro"
+  subnet_id               = ""
+  vpc_subnets             = []
+  vpc_security_group_ids  = []
+  user_security_groups    = []
+  encrypted               = false
+  key_name                = module.keypair_f8bedb.key_name
+  user_data = <<-EOF
+  #!/bin/bash
+  yum update -y
+  yum install -y httpd
+  systemctl start httpd
+  systemctl enable httpd
+  echo "<h1>Hello World from $(hostname -f)</h1>" > /var/www/html/index.html
+  EOF
+}
+
+output "ec2_instance_id_f8bedb" { value = module.ec2_instance_f8bedb.instance_id }
+output "ec2_public_ip_f8bedb"    { value = module.ec2_instance_f8bedb.public_ip }
+output "ec2_private_ip_f8bedb"   { value = module.ec2_instance_f8bedb.private_ip }
