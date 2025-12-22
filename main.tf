1b422cc4cf96a4388de1d1732feba5eee4f53de6
Abrar Syed <sabrarsayeed@gmail.com>
2025-12-22 10:17:57 +0000

    Launched a prod server

--- main.tf
+++ main.tf
@@ -1,9 +1,5 @@
 
 
-provider "aws" {
-  region = "us-east-1"
-}
-
 module "keypair_0c319f" {
   source                  = "./modules/keypair_0c319f"
   project_name            = "GenAI"
@@ -36,3 +32,41 @@ module "ec2_instance_0c319f" {
 output "ec2_instance_id_0c319f" { value = module.ec2_instance_0c319f.instance_id }
 output "ec2_public_ip_0c319f"    { value = module.ec2_instance_0c319f.public_ip }
 output "ec2_private_ip_0c319f"   { value = module.ec2_instance_0c319f.private_ip }
+
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+module "keypair_5e66c4" {
+  source                  = "./modules/keypair_5e66c4"
+  project_name            = "GenAI"
+  environment             = "dev"
+  name                    = "GenAI-keypair-5e66c4"
+}
+
+module "ec2_instance_5e66c4" {
+  source                  = "./modules/ec2_5e66c4"
+  project_name            = "GenAI"
+  environment             = "dev"
+  name                    = "GenAI-ec2-5e66c4"
+  instance_type           = "t2.micro"
+  subnet_id               = ""
+  vpc_subnets             = []
+  vpc_security_group_ids  = []
+  user_security_groups    = []
+  encrypted               = false
+  key_name                = module.keypair_5e66c4.key_name
+  user_data = <<-EOF
+  #!/bin/bash
+  yum update -y
+  yum install -y httpd
+  systemctl start httpd
+  systemctl enable httpd
+  echo "<h1>Hello World from $(hostname -f)</h1>" > /var/www/html/index.html
+  EOF
+}
+
+output "ec2_instance_id_5e66c4" { value = module.ec2_instance_5e66c4.instance_id }
+output "ec2_public_ip_5e66c4"    { value = module.ec2_instance_5e66c4.public_ip }
+output "ec2_private_ip_5e66c4"   { value = module.ec2_instance_5e66c4.private_ip }
