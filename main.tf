05e20be5764e3e8f48989ab306ff231cb6c087a4
Abrar Syed <sabrarsayeed@gmail.com>
2025-12-22 10:04:29 +0000

    started a new prod server

--- main.tf
+++ main.tf
@@ -34,10 +34,6 @@ output "ec2_public_ip_399f8f"    { value = module.ec2_instance_399f8f.public_ip 
 output "ec2_private_ip_399f8f"   { value = module.ec2_instance_399f8f.private_ip }
 
 
-provider "aws" {
-  region = "us-east-1"
-}
-
 module "keypair_eec1d9" {
   source                  = "./modules/keypair_eec1d9"
   project_name            = "GenAI"
@@ -70,3 +66,41 @@ module "ec2_instance_eec1d9" {
 output "ec2_instance_id_eec1d9" { value = module.ec2_instance_eec1d9.instance_id }
 output "ec2_public_ip_eec1d9"    { value = module.ec2_instance_eec1d9.public_ip }
 output "ec2_private_ip_eec1d9"   { value = module.ec2_instance_eec1d9.private_ip }
+
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+module "keypair_528a31" {
+  source                  = "./modules/keypair_528a31"
+  project_name            = "GenAI"
+  environment             = "dev"
+  name                    = "GenAI-keypair-528a31"
+}
+
+module "ec2_instance_528a31" {
+  source                  = "./modules/ec2_528a31"
+  project_name            = "GenAI"
+  environment             = "dev"
+  name                    = "GenAI-ec2-528a31"
+  instance_type           = "t3.micro"
+  subnet_id               = ""
+  vpc_subnets             = []
+  vpc_security_group_ids  = []
+  user_security_groups    = []
+  encrypted               = false
+  key_name                = module.keypair_528a31.key_name
+  user_data = <<-EOF
+  #!/bin/bash
+  yum update -y
+  yum install -y httpd
+  systemctl start httpd
+  systemctl enable httpd
+  echo "<h1>Hello World from $(hostname -f)</h1>" > /var/www/html/index.html
+  EOF
+}
+
+output "ec2_instance_id_528a31" { value = module.ec2_instance_528a31.instance_id }
+output "ec2_public_ip_528a31"    { value = module.ec2_instance_528a31.public_ip }
+output "ec2_private_ip_528a31"   { value = module.ec2_instance_528a31.private_ip }
